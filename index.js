10c87274c3bb05893d97b3818232e6f17b6f2b37
rtong123 <rbcatong@gmail.com>
2020-08-12 21:30:31 -0400

    added linkedin button

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function clickMe(){
+function emailMe(){
     window.open('mailto:rbcatong@gmail.com');
 }
 
+function linkMe(){
+    window.open("https://www.linkedin.com/in/rebecca-tong/");
+}
 
 function readMore(){
     let post3 = document.getElementById('post3')
