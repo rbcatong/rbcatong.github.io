ddb603eacb0abef42bf5c7853bd70b631c18f980
rtong123 <rbcatong@gmail.com>
2020-08-12 21:43:53 -0400

    added twitter buton

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function linkMe(){
     window.open("https://www.linkedin.com/in/rebecca-tong/");
 }
 
+function tweetMe(){
+    window.open("https://twitter.com/rbcatong");
+}
 function readMore(){
     let post3 = document.getElementById('post3')
     post3.style.display = "block";
 }
+
