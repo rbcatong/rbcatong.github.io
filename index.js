3db22a9192d7090d364ae2ed44ee618dac9af1ab
rbcatong <rbcatong@gmail.com>
2020-08-25 16:11:50 -0400

    changing navigaiton bar again

--- index.js
+++ index.js
@@ -26,9 +26,3 @@ function showResume(){
 }
 
 
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
-});
